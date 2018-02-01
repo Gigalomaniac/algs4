c54820196879cf8c05b464482bbb40b952189ffb
Tony Shaw <startong@outlook.com>
2018-02-01 22:37:01 +0800

    Optimise ex1.1.19

--- ch1/ex1_1_19.java
+++ ch1/ex1_1_19.java
@@ -2,9 +2,7 @@ public class ex1_1_19
 {
     public static long F(int N)
     {
-        if (N == 0) return 0;
-        if (N == 1) return 1;
-        long[] a = new long[N];
+        long[] a = new long[N+1];
         a[0] = 1;
         a[1] = 1;
         for (int i = 2; i < N; i++)
@@ -13,7 +11,7 @@ public class ex1_1_19
     }
     public static void main(String args[])
     {
-        for (int N = 0; N < 100; N++)
+        for (int N = 1; N < 93; N++)
             StdOut.println(N + " " + F(N));
     }
 }
