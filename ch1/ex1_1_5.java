187eb44dba2b90557b7c2fa299f5f07748a13da0
Tony Shaw <startong@outlook.com>
2018-02-01 11:00:15 +0800

    optimise ex1.1.5

--- ch1/ex1_1_5.java
+++ ch1/ex1_1_5.java
@@ -5,11 +5,6 @@ public class ex1_1_5
         StdOut.println("Please enter double values x and y.");
         double x = StdIn.readDouble();
         double y = StdIn.readDouble();
-        if (x > 0 && x < 1 && y > 0 && y < 1)
-            StdOut.println("true");
-        else
-            StdOut.println("false");
+        StdOut.println(x > 0 && x < 1 && y > 0 && y < 1);
     }
 }
-
-        
